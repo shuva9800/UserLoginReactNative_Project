d5a1c83c24eed74c9ff632f726d5e14817075e77
shuva9800 <ffshuva@gmail.com>
2024-09-25 20:43:34 +0530

    #

--- SignupLoginApp/app/index.jsx
+++ SignupLoginApp/app/index.jsx
@@ -1,21 +1,5 @@
-// import { Text, View } from "react-native";View
-
-// export default function Index() {
-//   return (
-//     <View
-//       style={{
-//         flex: 1,
-//         justifyContent: "center",
-//         alignItems: "center",
-//       }}
-//     >
-//       <Text>Edit app/index.tsx to edit this screen.</Text>
-//     </View>
-//   );
-// }
-
-import 'react-native-gesture-handler';
-import { registerRootComponent } from 'expo';
-import App from './navigation/AppNavigator';
+import "react-native-gesture-handler";
+import { registerRootComponent } from "expo";
+import App from "./navigation/AppNavigator";
 
 registerRootComponent(App);
--- SignupLoginApp/app/screens/HomeScreen.jsx
+++ SignupLoginApp/app/screens/HomeScreen.jsx
@@ -1,33 +1,22 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-import { HelloWave } from '@/components/HelloWave';
-// import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { View,Text } from 'react-native-web';
-// import { ThemedText } from '@/components/ThemedText';
-// import { ThemedView } from '@/components/ThemedView';
+import { Image, StyleSheet, Platform } from "react-native";
+import { HelloWave } from "@/components/HelloWave";
+
+import { View, Text } from "react-native-web";
 
 export default function HomeScreen({ navigation }) {
   return (
     <View
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require("@/assets/images/partial-react-logo.png")}
           style={styles.reactLogo}
         />
-      }>
-      
-      {/* <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView> */}
+      }
+    >
       <View style={styles.stepContainer}>
-        {/* <ThemedText type="subtitle">Step 1: Try it</ThemedText> */}
-        <Text onPress={() => navigation.navigate('Signup')}>
-          Sign Up
-        </Text>
-        <Text onPress={() => navigation.navigate('Login')}>
-          Login
-        </Text>
+        <Text onPress={() => navigation.navigate("Signup")}>Sign Up</Text>
+        <Text onPress={() => navigation.navigate("Login")}>Login</Text>
       </View>
     </View>
   );
@@ -35,17 +24,17 @@ export default function HomeScreen({ navigation }) {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
     // gap: 8,
     marginBottom: 8,
-    display: 'flex',
-    flexDirection:'row',
-    justifyContent:'space-evenly',
-   
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+
     // maxWidth:'50%',
   },
   reactLogo: {
@@ -53,6 +42,6 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
